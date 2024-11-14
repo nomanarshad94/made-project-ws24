aa4f927e77d3a28f940929bdc0457a0f41bf9c45
Noman Arshad <nomanarshad94@yahoo.com>
2024-11-14 14:11:29 +0100

    kaggle details added

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-py -3.7 -W ignore ../project/etl_pipeline.py
+py -3.7 -W ignore ../project/etl_pipeline_improved.py
 
 # As I have multiple python environments so only environement with python 3.7 contains required packages of python for this project.
 # So I have to run pipeline.py file using py -3.7 instead of python3
 # with -W ignore I am supressing warnings from python
 # I have created a requirement.txt file with all the required packages need to be installed in python using pip before running pipeline
+# lastly I am using kaggle api for dataset. kaggle api requires kaggle.json with valid username and api key to fetch the data. Due to security
+# reasons I am not pushing api key and username to github repo. But to run the pipeline you have to have kaggle.json file in .kaggle folder under
+# your user directory (~/.kaggle/kaggle.json on Linux and C:\Users\<Windows-username>\.kaggle\kaggle.json on Windows)
